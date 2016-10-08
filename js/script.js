dcf2978b77133a000044e090779b81f48a471d21
Ilya91 <webmaster.1991@mail.ru>
2016-10-08 11:15:26 +0300

    скипт для модального окна авторизации

---
+++ js/script.js
@@ -0,0 +1,49 @@
+var link = document.querySelector(".login");
+var modalLogin = document.querySelector(".modal-content");
+var close = document.querySelector(".modal-content-close");
+var overlay = document.querySelector(".modal-overlay");
+var login = modalLogin.querySelector("[name=login]");
+var password = modalLogin.querySelector("[name=password]");
+var form = modalLogin.querySelector("form");
+var storage = localStorage.getItem("login");
+
+
+link.addEventListener("click", function (e) {
+    e.preventDefault();
+    modalLogin.classList.add("modal-content-show");
+    overlay.classList.add("overlay-content-show");
+
+    if(storage){
+        login.value = storage;
+        password.focus();
+    }else{
+        login.focus();
+    }
+});
+close.addEventListener("click", function (e) {
+    e.preventDefault();
+    modalLogin.classList.remove("modal-content-show");
+    modalLogin.classList.remove("modal-error");
+    overlay.classList.remove("overlay-content-show");
+});
+overlay.addEventListener("click", function () {
+    modalLogin.classList.remove("modal-content-show");
+    overlay.classList.remove("overlay-content-show");
+});
+form.addEventListener("submit", function (e) {
+
+    if (!login.value || !password.value){
+        e.preventDefault();
+        modalLogin.classList.add("modal-error");
+    }else {
+        localStorage.setItem("login", login.value);
+    }
+});
+
+window.addEventListener("keydown", function (e) {
+    if (e.keyCode === 27){
+        modalLogin.classList.remove("modal-content-show");
+        overlay.classList.remove("modal-content-show");
+        overlay.classList.remove("modal-error");
+    }
+});
